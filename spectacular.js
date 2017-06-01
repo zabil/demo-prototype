8d878853773e437dc1f4e49372a3535bffa4924d
Zabil Cheriya Maliackal <zabilcm@gmail.com>
2017-06-01 20:47:34 +0530

    Add route for matching lines according to a filter

--- spectacular.js
+++ spectacular.js
@@ -8,11 +8,17 @@ if (process.argv.length != 3) {
 }
 
 var express = require('express')
+
 var app = express()
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // for parsing application/json
+
 var path = require('path');
 var fs = require('fs');
+var unique = require('array-unique');
 
-var specifications_folder = process.argv.pop();
+var specifications_folder = path.join(__dirname + '/' + process.argv.pop());
 
 app.use(express.static('public'));
 
@@ -21,8 +27,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/specifications', function (req, res) {
-    fs.readdir(path.join(__dirname + '/' + specifications_folder), function (err, files) {
-        console.log(files);
+    fs.readdir(specifications_folder, function (err, files) {
         res.setHeader('Content-Type', 'application/json');
         res.send(files);
     });
@@ -31,9 +36,23 @@ app.get('/specifications', function (req, res) {
 app.get('/specification/:file', function (req, res) {
     fs.readFile(path.join(__dirname + '/specs/' + req.params.file), 'utf8', function (err, data) {
         res.setHeader('Content-Type', 'text/markdown');
-        console.log(data);
         res.send(data);
     })
 });
+
+var grep = require('simple-grep');
+
+app.post('/steps', function (req, res) {
+    var lines = [];
+    grep(req.body.filter, specifications_folder, function (matches) {
+        matches.forEach(function (match) {
+            match.results.forEach(function (result) {
+                lines.push(result.line);
+            })
+        })
+        res.send(unique(lines));
+    });
+});
+
 app.listen(3000);
-console.log("Listening at http://localhost:3000");
+console.log("Goto http://localhost:3000");
